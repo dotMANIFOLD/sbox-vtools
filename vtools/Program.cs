e43b340baa0f190c0c9286b69762652f214d274a
dotRYEN <work@dotryen.me>
2025-06-10 13:33:30 -0400

    small fix for publishing

--- vtools/Program.cs
+++ vtools/Program.cs
@@ -5,6 +5,7 @@ namespace MANIFOLD;
 class Program {
     static async Task<int> Main() {
         return await new CliApplicationBuilder()
+            .SetExecutableName("vtools")
             .AddCommandsFromThisAssembly()
             .Build()
             .RunAsync();
