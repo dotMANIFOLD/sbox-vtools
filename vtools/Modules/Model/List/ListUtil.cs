5581cf34d3933c4c12b87d9de5e64c1d43e75440
dotRYEN <work@dotryen.me>
2025-10-31 23:24:59 -0400

    model: add null check to list weights

--- vtools/Modules/Model/List/ListUtil.cs
+++ vtools/Modules/Model/List/ListUtil.cs
@@ -5,7 +5,8 @@ namespace MANIFOLD.VTools.Model {
         public static IEnumerable<string> ListAllWeightLists(KV3File file) {
             var rootNode = file.Root.GetProperty<KVObject>("rootNode");
             var typeLists = rootNode.GetArray("children");
-            var list = typeLists.First(x => x.GetStringProperty("_class") == "WeightListList");
+            var list = typeLists.FirstOrDefault(x => x.GetStringProperty("_class") == "WeightListList");
+            if (list == null) return [];
             var masks = list.GetArray("children");
             return masks.Select(x => x.GetStringProperty("name"));
         }
